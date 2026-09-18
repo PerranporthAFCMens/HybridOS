ce371e0f866051b53ee4325dc2d795b0fe4b4281
PerranporthAFCMens <shortcutsemail@gmail.com>
2026-09-18 13:05:59 +0100

    Keep admin navigation free of white loading mask

--- app-stability.js
+++ app-stability.js
@@ -72,6 +72,10 @@
       if(u.origin!==location.origin)return;
       if(u.pathname===location.pathname&&u.search===location.search)return;
       if(!/\.html$|\/$/.test(u.pathname))return;
+      var adminFiles=['index.html','community.html','classes.html','class-setup.html','admin-operations.html','resource-availability.html','staff-permissions.html','access-settings.html','reporting.html','member-view-settings.html'];
+      var currentFile=location.pathname.split('/').pop()||'index.html';
+      var targetFile=u.pathname.split('/').pop()||'index.html';
+      if(adminFiles.indexOf(currentFile)!==-1&&adminFiles.indexOf(targetFile)!==-1)return;
       beginNavigation();
     }catch(_e){}
   },true);
